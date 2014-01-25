87eb1ce7992828b54488407f74a6a1c068325e1f
Philip Horger <campadrenalin@gmail.com>
2014-01-25 12:11:48 -0800

    Everything working except IP detection and custom titles

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"github.com/codegangsta/martini"
+	"github.com/codegangsta/martini-contrib/render"
+)
+
+type Template struct {
+	Handlers []string
+	Tmpl     string
+	Title    string
+}
+
+func setupTemplate(m *martini.ClassicMartini, t Template) {
+	handler := func(r render.Render) {
+		r.HTML(200, t.Tmpl, t.Title)
+	}
+	for _, name := range t.Handlers {
+		m.Get("/"+name, handler)
+	}
+}
+
+func newTemplate(tmpl string, title string) Template {
+	return Template{
+		Handlers: []string{tmpl},
+		Tmpl:     tmpl,
+		Title:    title,
+	}
+}
+
+func main() {
+	templates := []Template{
+		Template{
+			Handlers: []string{"", "index", "index.htm", "index.html"},
+			Tmpl:     "index",
+			Title:    "Home",
+		},
+		newTemplate("availability", "Goodies"),
+		newTemplate("contact", "Goodies"),
+		newTemplate("faq", "Goodies"),
+		newTemplate("goodies", "Goodies"),
+		newTemplate("hardware", "Goodies"),
+		newTemplate("pricing", "Goodies"),
+	}
+
+	m := martini.Classic()
+	m.Use(render.Renderer(render.Options{
+		Layout: "layout",
+	}))
+	m.Use(martini.Static("root"))
+
+	for _, t := range templates {
+		setupTemplate(m, t)
+	}
+
+	m.Run()
+}
